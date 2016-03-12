554dd7976f5f823012a6536f202ab5d4792902ee
Philip Boulain <LionsPhil@users.noreply.github.com>
2016-03-12 15:54:30 +0000

    Remove high/low missile support
    Remove all the horrible code to deal with having a single Lua controller deal with both missiles or torpedoes, which has never been tested. The much better way to have torpedo support would be with a separate Lua controller block and having this script restrict itself to given weapon groups, once a way to do that exists or can be found.
    
    Also replaces half_pi with a tunable in the process of necessary renames.

--- missile.lua
+++ missile.lua
@@ -4,10 +4,7 @@ Copyright 2016 Philip Boulain. Licensed under the ISC License.
 
 Shortcomings:
   - Want a way to restrict this to affect just a single weapon group, then you
-      can have one per missile type and drop the whole high/low thing.
-  - Assumes the role of a missile is defined by its current height over sea
-      level; must ensure air/surface missiles are catapulted above the water
-      before the guidance will take effect. (Would be nice to get design info.)
+      can have one per missile type (torpedoes in particular need it).
   - Missiles are not sticky, although target-switching is discouraged by their
       estimation of if they can reach an alternative. Can perform some pretty
       vexing swerve-to-misses if priorities are unstable.
@@ -16,57 +13,56 @@ Shortcomings:
   - Pretty CPU-intensive, does a lot of recomputation every update.
 ]]--
 
--- Tunables (high = SAM/SSM/AGM; low = torpedo) --------------------------------
--- Jump/dive depth for missiles, allowing them to cross-target
-sea_crossover_tolerance = 10
+-- Tunables --------------------------------------------------------------------
+-- Are the missiles torpedos? Will try to stay above/below sea as needed.
+is_torpedo               = false
 -- Distance beyond which won't even *try* to persue targets (meters)
-high_maximum_range      = 1000
-low_maximum_range       = 5000
+maximum_range            = 1000
 -- Rate at which missile can turn (radians/second; use measurement mode!)
-high_turn_rate          = 0.54
-low_turn_rate           = math.rad(10)
+turn_rate                = 0.54
 -- Estimated speed the missile will spend most of its lifespan at (m/s)
 -- (use measurement mode!)
-high_speed_estimate     = 115
-low_speed_estimate      = 50
+speed_estimate           = 115
 -- If within this range, but now overshooting the target, detonate (meters)
 -- (Like a proximity fuse, but will always try to get as close as possible)
 -- Target block changes can make this ineffective at large values
-high_prox_abort         = 3
-low_prox_abort          = 3
--- High missiles will avoid going below this height until they are close enough
+prox_abort               = 3
+-- Air missiles will avoid going below this height until they are close enough
 -- that they have to turn into their target (not strictly skimming, since they
 -- won't make effort to dive to it either) (meters)
-sea_skim_height         = 2
+sea_skim_height          = 2
 -- If no target and younger than this, climb; otherwise cruise (seconds)
 -- Setting this too high can cause missiles to swerve away if they are about
 -- to slam down on a ship when the targetted block changes beyond their ability
 -- to turn.
-max_climb_age           = 1
+max_climb_age            = 1
+-- Angle beyond which all targets are considered equally off-course. Wider thus
+-- means the targetting decision will be dominated by where the missile is
+-- already facing, even if it's an otherwise inferior target. Too narrow will
+-- instead make missiles very indecisive. (Radians; note that the *cone* of
+-- directional sensitive will be twice this, from side to side.)
+off_course_clamp         = math.rad(45)
 -- Spam the Lua block log with de-bugging messages
-dbg_spam                = false
+dbg_spam                 = false
 -- Spam the HUD when we do something cool
-hud_spam                = true
+hud_spam                 = true
 -- Measurement mode: DON'T TRACK TARGETS, just do some missile acrobatics and
 -- log values for the above to the Lua block. To use this, turn it on, fire
 -- ONLY ONE MISSILE (rip up some launchpads if you have to), wait for the HUD
 -- message, then go to the Lua block log and copy the measurments into the
 -- tunables above and turn this back off (...and restore any launchpads).
 -- You must save-and-run the code to reset state for each measurement run.
-measurement_mode        = false
+measurement_mode         = false
 -- How long to give the missile to clear the vessel before measuring.
 -- Particularly important for submarine-launched missiles that have to clear
 -- the surface.
-measurement_mode_start  = 0.5
+measurement_mode_start   = 0.5
 -- How much of a turn to measure. Normal missiles should be fine one quarter.
-measurement_mode_turn   = math.rad(90)
+measurement_mode_turn    = math.rad(90)
 -- How long a missile can try to make the turn before measurements give up.
 -- Useful to clean up for subsequent retries.
-measurement_mode_timeout= 10
+measurement_mode_timeout = 10
 --------------------------------------------------------------------------------
-
--- TODO only used for focusing missiles; make this a 'blinkers' tunable
-half_pi = math.pi * 0.5
 
 mm_start_vector = nil
 mm_complete     = false
@@ -123,7 +119,7 @@ end
 
 -- Returns angle in radians between missile facing and direction to target
 function AngleToTarget(
-  I, missile_info, missile_high, target, target_in_missile_coords)
+  I, missile_info, target, target_in_missile_coords)
 
   if target_in_missile_coords == nil then
     target_in_missile_coords = target.AimPointPosition - missile_info.Position
@@ -137,7 +133,7 @@ end
 -- Currently very dumb and (mostly) ignores that both are moving, let alone
 -- accellerating.
 function TimeToTarget(
-  I, missile_info, missile_high, target, target_in_missile_coords)
+  I, missile_info, target, target_in_missile_coords)
 
   if target_in_missile_coords == nil then
     target_in_missile_coords = target.AimPointPosition - missile_info.Position
@@ -146,20 +142,16 @@ function TimeToTarget(
   -- Early within the missile's life, velocity is changing rapidly; use the
   -- speed-at-turn as a minimum to assume we'll accellerate to it
   local missile_speed = missile_info.Velocity.magnitude
-  if missile_high and missile_speed < high_speed_estimate
-                 then missile_speed = high_speed_estimate end
-  if missile_low  and missile_speed <  low_speed_estimate
-                 then missile_speed =  low_speed_estimate end
+  if missile_speed < speed_estimate
+    then missile_speed = speed_estimate end
 
   -- Estimate time-to-target in the most trivial case
   local time_to_target = target_in_missile_coords.magnitude / missile_speed
 
   -- The more off-course we are, the longer we'll take
-  local turn_rate = low_turn_rate
-  if missile_high then turn_rate = high_turn_rate end
   -- TODO Rework how *ToTarget functions are called to avoid recomputation here
   local angle_to_target = AngleToTarget(
-    I, missile_info, missile_high, target, target_in_missile_coords)
+    I, missile_info, target, target_in_missile_coords)
   -- Add the time needed to make the turn, factoring in time wasted travelling
   -- in the wrong direction. First term is simply time to rotate the missile;
   -- multiplied by a factor of how much of this time is spent not closing, which
@@ -172,14 +164,7 @@ function TimeToTarget(
 end
 
 -- Returns boolean indicating if success is possible to intercept this target
-function MissileCanHit(I, missile_info, missile_high, target)
-  local maximum_range = low_maximum_range
-  local turn_rate     = low_turn_rate
-  if missile_high then
-    maximum_range = high_maximum_range
-    turn_rate     = high_turn_rate
-  end
-
+function MissileCanHit(I, missile_info, target)
   local target_in_missile_coords =
     target.AimPointPosition - missile_info.Position
   local distance_to_target = target_in_missile_coords.magnitude
@@ -195,9 +180,9 @@ function MissileCanHit(I, missile_info, missile_high, target)
 
   -- Is it within our turning circle? (Can we turn X degrees in Y distance?)
   local angle_to_target = AngleToTarget(
-    I, missile_info, missile_high, target, target_in_missile_coords)
+    I, missile_info, target, target_in_missile_coords)
   local time_to_target  = TimeToTarget(
-    I, missile_info, missile_high, target, target_in_missile_coords)
+    I, missile_info, target, target_in_missile_coords)
   if angle_to_target > turn_rate * time_to_target then
     if dbg_spam then I:Log(string.format(
       "Missile %d can't reach target %d by turning %gdeg in %gs",
@@ -209,7 +194,7 @@ function MissileCanHit(I, missile_info, missile_high, target)
   return true
 end
 
-function BestTargetForMissile(I, missile_info, missile_high, targets)
+function BestTargetForMissile(I, missile_info, targets)
   -- Sort the targets by how desirable they are
   -- ("Lesser" here means better: comes early in the sort results)
   -- (Wanted: std::partial_sort)
@@ -229,8 +214,8 @@ function BestTargetForMissile(I, missile_info, missile_high, targets)
     local a_angle = math.acos(Vector3.Dot(missile_normal, a_normal))
     local b_normal = (b.AimPointPosition - missile_info.Position).normalized
     local b_angle = math.acos(Vector3.Dot(missile_normal, b_normal))
-    if a_angle > half_pi then a_angle = half_pi end
-    if b_angle > half_pi then b_angle = half_pi end
+    if a_angle > off_course_clamp then a_angle = off_course_clamp end
+    if b_angle > off_course_clamp then b_angle = off_course_clamp end
     if a_angle < b_angle then return true  end
     if b_angle < a_angle then return false end
 
@@ -254,7 +239,7 @@ function BestTargetForMissile(I, missile_info, missile_high, targets)
 
   -- Find the best target we can hit
   for ignore, target in ipairs(targets) do
-    if MissileCanHit(I, missile_info, missile_high, target) then
+    if MissileCanHit(I, missile_info, target) then
       return target
     end
   end
@@ -264,30 +249,23 @@ function BestTargetForMissile(I, missile_info, missile_high, targets)
 end
 
 function Update(I)
-  -- Target locations
-  local high_targets = {} -- for air/surface missiles
-  local low_targets  = {} -- for torpedos
-
   -- Get some targets
-  local mainframes = I:GetNumberOfMainframes()
-  for mainframe = 0, mainframes-1 do
-    local targets = I:GetNumberOfTargets(mainframe)
-    for target = 0, targets-1 do
+  local targets = {}
+  local mainframe_count = I:GetNumberOfMainframes()
+  for mainframe = 0, mainframe_count-1 do
+    local target_count = I:GetNumberOfTargets(mainframe)
+    for target = 0, target_count-1 do
       local target_info = I:GetTargetInfo(mainframe, target)
       local target_position_info = I:GetTargetPositionInfo(mainframe, target)
       if target_info.Valid and target_position_info.Valid then
-        if target_position_info.AltitudeAboveSeaLevel > -sea_crossover_tolerance
-          then table.insert(high_targets, target_info) end
-        if target_position_info.AltitudeAboveSeaLevel <  sea_crossover_tolerance
-          then table.insert( low_targets, target_info) end
-      end
+        table.insert(targets, target_info) end
     end
   end
 
   -- Get and aim our missiles
   local already_measuring = false
-  local transcievers = I:GetLuaTransceiverCount()
-  for transciever = 0, transcievers-1 do
+  local transciever_count = I:GetLuaTransceiverCount()
+  for transciever = 0, transciever_count-1 do
     local missiles = I:GetLuaControlledMissileCount(transciever)
     for missile = 0, missiles-1 do
       local missile_info = I:GetLuaControlledMissileInfo(transciever, missile)
@@ -307,20 +285,14 @@ function Update(I)
 
       else -- not measurement mode
 
-        -- Don't appear to be able to get height over sea-level for a
-        -- missile_info, but sea is currently always the plane y == 0
-        local missile_high = false
-        local targets = low_targets
-        if missile_info.Position.y >= 0 then
-          missile_high = true
-          targets = high_targets
-        end
         local best_target = BestTargetForMissile(
-          I, missile_info, missile_high, targets)
+          I, missile_info, targets)
 
         if best_target == nil then
           -- Nothing we can hit!
-          if missile_high and missile_info.TimeSinceLaunch < max_climb_age then
+          if not is_torpedo
+            and missile_info.TimeSinceLaunch < max_climb_age then
+
             -- Gain altitude, make our turn easier
             local climb = missile_info.Position
             climb.y = climb.y + 1000000 -- will cruise toward this; make it high
@@ -333,22 +305,21 @@ function Update(I)
           -- We have a target!
           local aim_at = best_target.AimPointPosition
 
-          local prox_abort = low_prox_abort
-          if missile_high then prox_abort = high_prox_abort end
-
           local target_in_missile_coords = aim_at - missile_info.Position
 
           -- TODO use target and own Velocity to aim at intercept point
 
+          -- Don't appear to be able to get height over sea-level for a
+          -- missile_info, but sea is currently always the plane y == 0.
           -- If our target is underwater, stay dry and fast until the last
-          -- moment
-          if missile_high and aim_at.y < 0 then
+          -- moment.
+          if not is_torpedo and aim_at.y < 0 then
             local angle_to_target = AngleToTarget(
-              I, missile_info, missile_high, target, target_in_missile_coords)
+              I, missile_info, target, target_in_missile_coords)
             local time_to_turn = TimeToTarget(
-              I, missile_info, missile_high, target, target_in_missile_coords)
+              I, missile_info, target, target_in_missile_coords)
               * 0.5 -- get ready early so we don't abort
-            if angle_to_target < high_turn_rate * time_to_turn then
+            if angle_to_target < turn_rate * time_to_turn then
               -- Can still make the turn later
               aim_at.y = sea_skim_height
               if dbg_spam then I:Log(
